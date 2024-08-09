8feddc28e40bddd71fee92f0db11879cc8659b2f
mohammedsalman9562 <salmanasna149@gmail.com>
2024-08-09 09:53:34 +0530

    amazon

---
+++ components/header/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+import logo from "../../images/logo.png"
+
+const Header =() =>{
+    return (
+        <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50">
+            <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 p-4">
+            {/*logo*/}
+            <div>
+                <Image className="w-28 object-cover" src={logo} alt={"logoimg"}/>
+            </div>
+            {/*delivery*/}
+            {/*searchbar*/}
+            {/*signin*/}
+            {/*favrate*/}
+            {/*cart*/}
+            </div>
+        </div>
+    )
+}
+export default Header;
