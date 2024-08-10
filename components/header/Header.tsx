d6a1cda32badf2391bf8bbb3bcc38a833a933c57
mohammedsalman9562 <salmanasna149@gmail.com>
2024-08-10 19:47:12 +0530

    navbar completed

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 import Image from "next/image";
-import logo from "../../images/logo.png"
+import logo from "../../images/logo.png";
+import cart from "../../images/cart.png"
+
+import { CiLocationOn } from "react-icons/ci";
+import { HiOutlineSearch } from "react-icons/hi";
+import { BiCaretDown } from "react-icons/bi";
 
 const Header =() =>{
     return (
-        <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50">
-            <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 p-4">
+        <div className="w-full 
+        h-20  
+        bg-amazon_blue 
+        text-lightText 
+        sticky 
+        top-0 
+        
+        rz-50">
+            <div className="h-full  
+            w-full  
+            mx-auto  
+            inline-flex 
+            items-center 
+            justify-between 
+            gap-1 
+            mdl:gap-3 
+            p-4">
             {/*logo*/}
             <div className="px-2 border 
             border-transparent hover:border-white 
@@ -13,17 +33,66 @@ const Header =() =>{
             duration-300 
             items-center 
             justify-center
-            h-14 w-35"
-            >
+            h-14 w-35">
                 <Image className="w-28 object-cover" src={logo} alt={"logoimg"}/>
             </div>
             {/*delivery*/}
+            <div className="px-2 border 
+            border-transparent hover:border-white 
+            cursor-pointer 
+            duration-300 
+            items-center 
+            justify-center
+            h-14 w-35">
+                <CiLocationOn />
+                <div className="text-xs">
+                    <p>Deliver to</p>
+                    <p className="text-white 
+                    font-bold 
+                    uppercase">
+                        India</p>
+                </div>
+            </div>
             {/*searchbar*/}
+            <div className="flex-1 h-10 hidden md:inline-flex items-center
+            justify-between relative">
+                <input className="w-full h-full rounded-md px-2 placeholder:text-sm text-base text-black border-[3px] border-transparent outline-none
+                focus-visible:border-amazon_yellow" type="text" placeholder="Search amazon products"/>
+                <span className="w-12 h-full bg-amazon_yellow text-black text-2xl flex
+                items-center justify-center absolute right-0 rounded-md rounded-br-md">
+                    <HiOutlineSearch/>
+                </span>
+            </div>
             {/*signin*/}
-            {/*favrate*/}
+            <div className="px-2 border 
+            border-transparent hover:border-white 
+            cursor-pointer 
+            duration-300 
+            items-center 
+            justify-center
+            h-14 w-35">
+                <p>Hello, sign in</p>
+                <p className="text-white font-bold flex items-center ">Account & Lists{""}
+                    <span><BiCaretDown/></span>
+                </p>
+            </div>
+            {/*favorite*/}
+            <div className="px-2 border 
+            border-transparent hover:border-white 
+            cursor-pointer 
+            duration-300 
+            items-center 
+            justify-center
+            h-14 w-35">
+                <p>Marked</p>
+                <p className="text-white font-bold flex items-center ">& Favorites</p>
+            </div>
             {/*cart*/}
+            <div>
+                <Image className="w-auto object-cover h-8" src={cart} alt="cartimg"/>
+            </div>
             </div>
         </div>
-    )
+    );
 }
 export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
-  <Component {...pageProps} />;
+  <Component {...pageProps} />
   </div>
   )
 }
